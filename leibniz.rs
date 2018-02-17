2d1e9db4137fc67914f342a7d43864cb6e60cd6a
Niklas Heer <niklas.heer@gmail.com>
2018-02-17 20:05:33 +0100

    Add Rust example

---
+++ leibniz.rs
@@ -0,0 +1,24 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let mut file = File::open("./rounds.txt").expect("file not found");
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    let rounds = &contents.trim().parse::<i32>().unwrap();
+
+    let mut x: f64 = 1.0;
+    let mut pi: f64 = 1.0;
+
+    for i in 2..(rounds + 2) {
+        x *= -1.0;
+        pi = pi + (x / (2.0 * i as f64 - 1.0));
+    }
+
+    pi = pi * 4.0;
+
+    println!("π = {}", pi);
+}
