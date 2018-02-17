5d7fdcc1f238d2e6412af5789764304d16d94de5
Niklas Heer <niklas.heer@gmail.com>
2018-02-17 20:05:16 +0100

    Add python example

---
+++ leibniz.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding=utf-8
+import os
+
+
+with open("rounds.txt") as file:
+    rounds = int(file.read())
+
+x = 1
+pi = 1
+
+for i in range(2, rounds + 2):
+    x *= -1
+    pi += x / (2 * i - 1)
+
+pi *= 4
+
+print("π = " + str(pi))
