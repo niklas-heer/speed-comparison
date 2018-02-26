75d9f3da8d361b9e2e71ddbbfe668ec45ece4f80
Niklas Heer <niklas.heer@gmail.com>
2018-02-26 15:54:26 +0100

    Refactor big time and add pypy

--- comparison.py
+++ comparison.py
@@ -1,30 +1,25 @@
-#!/usr/bin/env python3
-# coding=utf-8
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module measures the performance of various programming languages."""
+
 import os
 import math
-import delegator
+import csv
 import logging
-from lauda import stopwatchcm
+import statistics
+import delegator
+from lauda import StopWatch
+from termcolor import colored
 
-
-def stopwatch_sum_cb(watch):
-    # Convert the measurement into milliseconds
-    time = int(watch.elapsed_time * 1000)
-    print("Time: {0}ms".format(time))
 
 class Measure:
     """Initializes the Measurement
 
-        Parameters
-        ----------
-            name : str
-                The name of the languages. Will be used as the headline.
-            version_cmd : str
-                The command as a string to get the version of the language/compiler.
-            run_cmd : str
-                The command as a string to run the program.
-            compile_cmd : str, optional
-                The command to compile the program first. Will be run before the `run_cmd`.
+        Arguments:
+            name : str  -- Name of the languages.
+            version_cmd : str -- Command to get the version.
+            run_cmd : str -- Command to run the program.
+            compile_cmd : str, optional -- Command to compile.
     """
 
     def __init__(self, name, version_cmd, run_cmd, compile_cmd=None, debug=False):
@@ -33,56 +28,159 @@ class Measure:
         self.version_cmd = version_cmd
         self.compile_cmd = compile_cmd
         self.debug = debug
+        self.results = {"name": self.name}
 
     def run(self):
-        print("> {0}".format(self.name))
+        """Run a measurement.
+
+        Returns:
+            list -- List of results.
+        """
+
+        print(f"> {self.name}")
 
         if self.compile_cmd:
             cmd_compile = delegator.run(self.compile_cmd)
             if self.debug or cmd_compile.return_code is not 0:
                 logging.info(cmd_compile.out)
 
-        print("Version: {0}".format(delegator.run(self.version_cmd).out.splitlines()[0]))
+        print(f"Version: {delegator.run(self.version_cmd).out.splitlines()[0]}")
 
-        with stopwatchcm(callback=stopwatch_sum_cb):
+        times = []
+        count = 0
+        while count < 10:
+            count += 1
+            # Measure execution time
+            watch = StopWatch()
+            watch.start()
             cmd_run = delegator.run(self.run_cmd)
+            watch.stop()
 
-        print("Result: π = {0}\n".format(cmd_run.out.rstrip()))
+            # Convert the measurement into milliseconds
+            times.append(int(watch.elapsed_time * 1000))
 
-def accuracy(expected, actual):
-    if expected < actual:
-        return (actual - expected) / expected
+        print(f"Speed (all): {'ms, '.join(map(str, times))}ms")
+        print(f"Speed (best): {min(times)}ms")
+        print(f"Speed (worst): {max(times)}ms")
+        print(f"Speed (median): {statistics.median(times)}ms")
+
+        # Strip output from new line
+        result = cmd_run.out.strip()
+        print(f"Result: {result}")
+
+        # Calculate accuracy
+        accuracy = self.diff_letters("{:.16f}".format(math.pi), result)
+        print("Accuracy: {:.2%}".format(accuracy))
+
+        print()  # new line
+
+        self.results["median"] = statistics.median(times)
+        self.results["best"] = min(times)
+        self.results["worst"] = max(times)
+        self.results["accuracy"] = f"{accuracy*100:.4}"
+
+        return self.results
+
+    def diff_letters(self, first, second):
+        """Measure differences between result and actual π.
+
+        Arguments:
+            first: str -- First number for comparison.
+            second : str -- Second number for comparison.
+        """
+
+        if self.debug:
+            # Print also a visual diff between the numbers
+            combined = zip(first, second)
+            diff = []
+            for i, j in combined:
+                if i == j:
+                    print(i, "--", j)
+                else:
+                    diff.append(j)
+                    print(i, "  ", j)
+            return(len(first) - len(diff)) / len(first)
+
+        # Standard diff algorithm
+        return (len(first) - sum(first[i] != second[i] for i in range(len(first)))) / len(first)
+
+
+def ask_yes_no(question, default="yes"):
+    """Helper function to ask a yes/no question.
+
+    Arguments:
+        question : str -- Question which will be displayed.
+        default : str, optional -- Set the default answer to the question.
+    Returns:
+        True if the answer is yes, else it returns False.
+    """
+    valid = {"yes": True, "y": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
     else:
-        return (expected - actual) / expected
+        raise ValueError("invalid default answer: '%s'" % default)
 
-def measure_accuracy():
-    # print("{:.16%}".format(accuracy(math.pi, pi_result)))
-    # -0.0000318309565063%
-    # -0.0000318309561953%
-    pass
+    while True:
+        choice = input(f"\n{colored('❓', 'red')}  {question}{prompt}").lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            print("Please respond with 'yes' or 'no'.\n")
 
 def measurement():
+    """Management function for the measurement.
+    """
+
     # Change the working directory to the `src` folder
     cwd = os.getcwd()
     os.chdir(os.path.join(cwd, "src"))
 
     with open("rounds.txt") as file:
-        rounds = file.read()
+        iterations = file.read().strip()
 
     print("\n======= Comparison =======")
-    print("Rounds used: {0}\n".format(rounds))
+    print(f"Iterations: {iterations}\n")
 
-    Measure("Julia", "julia --version", "julia leibniz.jl").run()
-    Measure("Python 3 (CPython)", "python3 --version", "python3 leibniz.py").run()
-    Measure("Ruby", "ruby --version", "ruby leibniz.rb").run()
-    Measure("Rust", "rustc --version", "./leibniz", "export RUST_BACKTRACE=1; rustc leibniz.rs").run()
-    Measure("JS (node.js)", "node --version", "node leibniz.js").run()
-    Measure("Lua", "lua5.3 -v", "lua5.3 leibniz.lua").run()
-    Measure("PHP", "php7 --version", "php7 leibniz.php").run()
-    Measure("Nim", "echo '0.17.2'", "./leibniz", "nim c --verbosity:0 leibniz.nim").run()  # printing nim --version is not possible
-    Measure("C++", "g++ --version", "./leibniz", "g++ leibniz.cpp -o leibniz").run()
-    Measure("Crystal", "crystal --version", "./leibniz", "crystal build leibniz.cr").run()
-    Measure("Go", "go version", "./leibniz", "go build leibniz.go").run()
-    Measure("C", "gcc --version", "./leibniz", "gcc leibniz.c -o leibniz").run()
+    languages = [
+        # language name / version command / execute command / compile command
+        ["Julia", "julia --version", "julia leibniz.jl"],
+        ["Python (CPython)", "python --version", "python leibniz.py"],
+        ["Ruby", "ruby --version", "ruby leibniz.rb"],
+        ["JS (node)", "node --version", "node leibniz.js"],
+        ["Python (pypy)", "pacman -Qi pypy | grep 'Version' | cut -d: -f2- | cut -d ' ' -f2", "pypy leibniz.py"],
+        ["Lua", "lua -v", "lua leibniz.lua"],
+        ["Rust", "rustc --version", "./leibniz", "export RUST_BACKTRACE=1; rustc leibniz.rs"],
+        ["PHP", "php --version", "php leibniz.php"],
+        ["Nim", "pacman -Qi nim | grep 'Version' | cut -d: -f2 | cut -d ' ' -f2", "./leibniz", "nim c --verbosity:0 leibniz.nim"],
+        ["C++", "g++ --version", "./leibniz", "g++ leibniz.cpp -o leibniz"],
+        ["Crystal", "crystal --version", "./leibniz", "crystal build leibniz.cr"],
+        ["Go", "go version", "./leibniz", "go build leibniz.go"],
+        ["C", "gcc --version", "./leibniz", "gcc leibniz.c -o leibniz"]
+    ]
+
+    complete_results = []
+
+    for language in languages:
+        complete_results.append(Measure(*language).run())
+
+    if ask_yes_no("Do you want to save the results?"):
+        # Write csv file with results
+        os.chdir(os.path.join(cwd, "results"))
+
+        keys = complete_results[0].keys()
+
+        with open(f"data.csv", "w", newline="", encoding="utf8") as file:
+            dict_writer = csv.DictWriter(file, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(complete_results)
+
+        print("Saved the results in: {0}".format(colored(f"results/data.csv", 'red', attrs=['bold'])))
 
 measurement()
