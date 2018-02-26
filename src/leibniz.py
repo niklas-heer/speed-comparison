484c9802296d1896201458bdb0a4855f24dcc0e1
Niklas Heer <niklas.heer@gmail.com>
2018-02-26 15:52:47 +0100

    Fix compatibility with pypy

--- src/leibniz.py
+++ src/leibniz.py
@@ -6,8 +6,8 @@ import os
 with open("rounds.txt") as file:
     rounds = int(file.read())
 
-x = 1
-pi = 1
+x = 1.0
+pi = 1.0
 
 for i in range(2, rounds + 2):
     x *= -1
@@ -15,5 +15,4 @@ for i in range(2, rounds + 2):
 
 pi *= 4
 
-print(pi)
-
+print("{:.16f}".format(pi))
