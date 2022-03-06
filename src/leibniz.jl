f1c46db5465e5c77a8bffd5fa2d3539e1abfc7e2
Oscar Smith <oscardssmith@gmail.com>
2022-03-06 10:30:01 -0600

    fix performance

--- src/leibniz.jl
+++ src/leibniz.jl
@@ -1,13 +1,15 @@
-file = open("rounds.txt")
-rounds = parse(Int64, strip(readstring(file)))
+function f(rounds)
+    x = 1.0
+    pi = 1.0
 
-x = 1.0
-pi = 1.0
+    for i in 2:(rounds + 2)
+        x *= -1
+        pi += x / (2 * i - 1)
+    end
 
-for i in 2:(rounds + 2)
-    x *= -1
-    pi += x / (2 * i - 1)
+    return pi*4
 end
 
-pi *= 4
-print(pi)
+file = open("rounds.txt")
+rounds = parse(Int64, strip(readstring(file)))
+print(f(rounds))
