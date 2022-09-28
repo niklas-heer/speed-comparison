2b88affc7cd955c77f2bbcba2579afc5eb39ad4e
Niklas Heer <me@nheer.io>
2022-09-28 19:08:15 +0200

    Wrap Python code into a function
    To address the concerns in #8 about Python, this should improve things.

--- src/leibniz.py
+++ src/leibniz.py
@@ -2,17 +2,20 @@
 # coding=utf-8
 import os
 
+# https://stackoverflow.com/questions/11241523/why-does-python-code-run-faster-in-a-function
+def main():
+    with open("rounds.txt") as file:
+        rounds = int(file.read())
 
-with open("rounds.txt") as file:
-    rounds = int(file.read())
+    x = 1.0
+    pi = 1.0
 
-x = 1.0
-pi = 1.0
+    for i in range(2, rounds + 2):
+        x *= -1
+        pi += x / (2 * i - 1)
 
-for i in range(2, rounds + 2):
-    x *= -1
-    pi += x / (2 * i - 1)
+    pi *= 4
 
-pi *= 4
-
-print("{:.16f}".format(pi))
+    print("{:.16f}".format(pi))
+    
+main()
