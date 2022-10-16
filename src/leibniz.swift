3cd101b86a1f76fb84b904a742258c2a9b5dbb94
Gabriel Baraldi <baraldigabriel@gmail.com>
2022-10-16 19:52:54 -0300

    Make swift compile

--- src/leibniz.swift
+++ src/leibniz.swift
@@ -1,16 +1,16 @@
-let rounds = 100000000
-
-
+import Foundation
+let text = try! String(contentsOfFile: "rounds.txt")
+let rounds = Int(text)!
 var x = 1.0
 var pi = 1.0
 
 for i in 2...rounds+2 {
-    x *= -1
-    pi += x / Double(2 * i - 1)
+    x *= -1.0
+    pi += x / (2.0 * Double(i) - 1.0)
 }
 
-pi *= 4
-print(pi)
+pi *= 4.0
+print(String(format: "%.16f", pi))
 
 // TODO:
 //  * Read rounds from file
