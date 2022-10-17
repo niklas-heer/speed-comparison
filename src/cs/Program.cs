203aa419b457063a9b49901dcc79ba872d6ea857
Niklas Heer <me@nheer.io>
2022-10-17 20:54:32 +0000

    ✨ Add C# version. Closes #28

---
+++ src/cs/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+
+var data = String.Empty;
+
+try {
+    data = File.ReadAllText("rounds.txt", System.Text.Encoding.UTF8);
+} catch (IOException err) {
+    Console.WriteLine($"Couldn't read file:\n {err.Message}");
+}
+
+int rounds = int.Parse(data.Replace("\n", "").Replace("\r", ""));
+
+double pi = 1;
+double x = 1;
+
+for (int i = 2; i < rounds + 2; i++) {
+    x *= -1;
+    pi += (x / (2 * i - 1));
+}
+
+pi *= 4;
+Console.WriteLine(pi);
