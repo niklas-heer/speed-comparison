22431c51397960367b94eec6bcf5ed8df437a96b
Francesco Alemanno <50984334+francescoalemanno@users.noreply.github.com>
2022-10-18 19:11:33 +0200

    faster fortran code
    it still evaluates all the terms, one by one.
    But it does so in a way that should lead to vector assembly code

--- src/leibniz.f90
+++ src/leibniz.f90
@@ -1,22 +1,30 @@
 program Leibniz
-   use, intrinsic :: iso_fortran_env, only: output_unit, error_unit, iostat_end, int64, real64
-   implicit none
-   integer(int64) :: i, file_unit, rc
-   integer(int64) :: rounds;
-   real(real64) :: x = 1.0_real64, pi = 1.0_real64
+    use, intrinsic :: iso_fortran_env, only: output_unit, error_unit, iostat_end, int64, real64
+    implicit none
+    integer(int64) :: i, file_unit, rc
+    integer(int64) :: rounds;
+    real(real64) :: pi0, pi1, pi
+ 
+    open (action='read', file="rounds.txt", iostat=rc, newunit=file_unit)
+    read (file_unit, *, iostat=rc) rounds
+    close (file_unit)
+    
+    pi0 = 0.0
+    pi1 = 0.0
 
-   open (action='read', file="rounds.txt", iostat=rc, newunit=file_unit)
-   read (file_unit, *, iostat=rc) rounds
-   close (file_unit)
-   
-   rounds = rounds + 2
+    do i=0, rounds, 4
+        pi0 = pi0 + term(i) - term(i+1)
+        pi1 = pi1 + term(i+2) - term(i+3)
+    enddo
+ 
+    pi = (pi0 + pi1) * 4
+    write(output_unit,*) pi
 
-   do i=2,rounds-1
-      x = -x
-      pi = pi + (x / (2 * i - 1))
-   enddo
-
-   pi = pi*4
-   write(output_unit,'(F20.18)') pi
-
-end program Leibniz
+    contains
+    
+    pure function term(n) result(t)
+        integer(int64), intent(in) :: n
+        real(real64) :: t
+        t = (1.0_real64 / (2 * n + 1))
+    end function term
+ end program Leibniz
