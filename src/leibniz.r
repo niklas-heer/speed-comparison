2671e9eb368e90a89b4bd2aac06de4d1576ddeb0
Jonathan Carroll <github@jcarroll.com.au>
2022-10-17 19:25:39 -0700

    one less multiplication

--- src/leibniz.r
+++ src/leibniz.r
@@ -1,5 +1,5 @@
 rounds <- scan("rounds.txt", what = integer(), quiet = TRUE)
   
-pi <- 4*sum(1 / seq.int(-2*rounds+1, 2*rounds, by = 4))
+pi <- sum(4 / seq.int(-2*rounds+1, 2*rounds, by = 4))
 
 cat(sprintf("%.16f\n", pi))
