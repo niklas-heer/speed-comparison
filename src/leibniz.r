6615dd9a6f9d4bb725a68d6e53f5ca8255cfddb7
Jonathan Carroll <github@jcarroll.com.au>
2022-10-17 19:22:26 -0700

    use scan() to read the input file

--- src/leibniz.r
+++ src/leibniz.r
@@ -1,5 +1,4 @@
-fileName <- "rounds.txt"
-rounds <- as.integer(readChar(fileName, file.info(fileName)$size))
+rounds <- scan("rounds.txt", what = integer(), quiet = TRUE)
   
 pi <- 4*sum(1 / seq.int(-2*rounds+1, 2*rounds, by = 4))
 
