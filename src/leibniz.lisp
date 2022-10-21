4e3d2dcb0f4b323745c9b756d4d78b8bba67cda1
motoshira <51407064+motoshira@users.noreply.github.com>
2022-10-22 01:14:21 +0900

    Add lisp

---
+++ src/leibniz.lisp
@@ -0,0 +1,19 @@
+(defun leibniz (rounds)
+  (declare (optimize (speed 3) (safety 0) (debug 0))
+           (fixnum rounds))
+  (let ((ans 1.0d0)
+        (sig 1.0d0)
+        (end (ash (1+ rounds) 1)))
+    (declare (double-float ans sig)
+             (fixnum end))
+    (loop for i of-type fixnum from 3 below end by 2
+          do (setf sig (- sig))
+             (incf ans (/ sig i)))
+    (* ans 4)))
+
+#-swank
+(with-open-file (in "rounds.txt")
+  (let ((*read-default-float-format* 'double-float)
+        (n (parse-integer (read-line in))))
+    (princ (leibniz n))
+    (fresh-line)))
