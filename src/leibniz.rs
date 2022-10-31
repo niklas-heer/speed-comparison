5cf020f192a3cf14b838d6a290950d848b2846be
Gaute Hope <gaute.hope@met.no>
2022-10-31 20:58:38 +0100

    rust: split between std features and rust nightly

--- src/leibniz.rs
+++ src/leibniz.rs
@@ -1,7 +1,5 @@
-#![feature(portable_simd)]
 use std::fs::File;
 use std::io::prelude::*;
-use std::simd::{SimdFloat, f64x8, u32x8};
 
 fn main() {
     let mut file = File::open("./rounds.txt").expect("file not found");
@@ -12,19 +10,10 @@ fn main() {
 
     let rounds = contents.trim().parse::<u32>().unwrap() + 2;
 
-    // We need to use f64 instead of f32 because otherwise the precision would be off
-
-    let x = f64x8::from_array([-1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0]);
-    let pi = f64x8::from_array([1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]);
-
-    let m2 = u32x8::splat(2);
-    let s2 = u32x8::splat(1);
-    let i2 = u32x8::from_array([0, 1, 2, 3, 4, 5, 6, 7]);
-
-    let pi = (2..rounds).step_by(8).fold(pi, |pi, i| {
-        let ii = u32x8::splat(i);
-        pi + x / (m2 * (ii + i2) - s2).cast::<f64>()
-    }).reduce_sum() * 4.0;
+    let pi = (2..rounds).fold(1.0, |pi, i| {
+        let x = -1.0f64 + (2.0 * (i & 0x1) as f64);
+        pi + x / (2 * i - 1) as f64
+    }) * 4.0;
 
     println!("{:.16}", pi);
 }
