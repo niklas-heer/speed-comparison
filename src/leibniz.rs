006e480c4892d490ef9fb11f7e1a819bebcd0620
Gaute Hope <gaute.hope@met.no>
2022-10-31 12:48:44 +0100

    simdify the rest

--- src/leibniz.rs
+++ src/leibniz.rs
@@ -1,7 +1,7 @@
 #![feature(portable_simd)]
 use std::fs::File;
 use std::io::prelude::*;
-use std::simd::{SimdFloat, f64x8};
+use std::simd::{SimdFloat, f64x8, u32x8};
 
 fn main() {
     let mut file = File::open("./rounds.txt").expect("file not found");
@@ -17,17 +17,13 @@ fn main() {
     let x = f64x8::from_array([-1.0, 1.0, -1.0, 1.0, -1.0, 1.0, -1.0, 1.0]);
     let pi = f64x8::from_array([1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]);
 
+    let m2 = u32x8::splat(2);
+    let s2 = u32x8::splat(1);
+    let i2 = u32x8::from_array([0, 1, 2, 3, 4, 5, 6, 7]);
+
     let pi = (2..rounds).step_by(8).fold(pi, |pi, i| {
-         pi + x / f64x8::from_array([
-            f64::from(2u32 * i - 1),
-            f64::from(2u32 * (i + 1) - 1),
-            f64::from(2u32 * (i + 2) - 1),
-            f64::from(2u32 * (i + 3) - 1),
-            f64::from(2u32 * (i + 4) - 1),
-            f64::from(2u32 * (i + 5) - 1),
-            f64::from(2u32 * (i + 6) - 1),
-            f64::from(2u32 * (i + 7) - 1),
-        ])
+        let ii = u32x8::splat(i);
+        pi + x / (m2 * (ii + i2) - s2).cast::<f64>()
     }).reduce_sum() * 4.0;
 
     println!("{:.16}", pi);
