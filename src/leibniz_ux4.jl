e96091341938bd4a39c02fb17dc6b931e7b5ba80
Páll Haraldsson <Pall.Haraldsson@gmail.com>
2022-11-13 18:49:09 +0000

    3.5x faster, same basic algorithm, a tine bit less accurate

--- src/leibniz_ux4.jl
+++ src/leibniz_ux4.jl
@@ -1,17 +1,18 @@
-function f(rounds)
-    pi = 1.0
-    x  = -1.0
+function f4(rounds)
+    pi = -1.0f0
+    # x  = -1.0
     r2 = rounds + 2
     vend = r2 - r2 % 4
-    @simd for i in 2:4:r2
-        pi +=   x / (2.0 * i -  1.0) - 
-		x / (2.0 * i +  1.0) + 
-		x / (2.0 * i +  3.0) - 
-		x / (2.0 * i +  5.0) 
+    @simd for i in 4:8:(r2*2)
+        pi += inv(i -  1.0f0) - 
+              inv(i +  1.0f0) + 
+              inv(i +  3.0f0) - 
+              inv(i +  5.0f0) 
     end
+    pi = -pi
     for i in vend+1:r2
-   	pi += 1.0 / (2.0 * (i + 0.0) - 1.0)
-	x = -x
+        pi += 1.0 / (2.0 * (i + 0.0) - 1.0)
+        # x = -x
     end
     return pi*4
 end
