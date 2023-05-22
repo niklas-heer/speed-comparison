894a9c795ee8ba4c72f890a806ca653c67c52ec7
Páll Haraldsson <Pall.Haraldsson@gmail.com>
2023-05-22 20:50:35 +0000

    Update leibniz.jl

--- src/leibniz.jl
+++ src/leibniz.jl
@@ -2,15 +2,15 @@ function f(rounds)
     pi = 1.0
     x  = -1.0
     r2 = rounds + 2
-    vend = r2 - r2 % 8
+    vend = Int64(r2 - r2) % 8
     @simd for i in 2*2:8*2:(r2*2)
-    # Common denominators method, half as many divisions:
+    # Common-denominators method, half as many divisions:
         pi += Float64(
                -2.0f0 / fma(i, i, -1.0f0) +
                # x / (2.0 * i + 1.0) +
-               -2.0f0 / (fma(i, i, 15.0f0) + 8f0i)
-               # x / (2.0 * i + 5.0)
-               -2.0f0 / (fma(i, i, 63f0) + 16f0i)
+               -2.0f0 / (fma(i, i, 15.0f0) + 8f0i) +
+               # x / (2.0 * i + 5.0) +
+               -2.0f0 / (fma(i, i, 63f0) + 16f0i) +
                # x / (2.0 * i + 9.0) +
                -2.0f0 / (fma(i, i, 143f0) + 24f0i)
                # x / (2.0 * i + 13.0)
@@ -18,9 +18,10 @@ function f(rounds)
     end
 
     for i in vend+1:r2
-        pi += x / (2.0 * (i + 0.0) - 1.0)
+        pi += x / (2.0f0 * i - 1.0f0)
         x = -x
     end
+
     return pi*4
 end
 
