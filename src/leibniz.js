508de0736d103840b3db490e02915fbb8452fcd8
Niklas Heer <me@nheer.io>
2025-12-05 01:45:37 +0100

    perf(js): add strict mode for potential performance improvement
    Addresses #119

--- src/leibniz.js
+++ src/leibniz.js
@@ -1,12 +1,14 @@
-let fs = require('fs');
-let rounds = parseInt(fs.readFileSync('./rounds.txt', 'utf8'));
+"use strict";
+
+let fs = require("fs");
+let rounds = parseInt(fs.readFileSync("./rounds.txt", "utf8"));
 
 let x = 1.0;
 let pi = 1.0;
 
 for (let i = 2; i < rounds + 2; i++) {
-    x *= -1;
-    pi += x / (2 * i - 1);
+  x *= -1;
+  pi += x / (2 * i - 1);
 }
 
 pi *= 4;
