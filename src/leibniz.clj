ceadfe20d11115fefe5e22b13bcf940b6a0457db
Niklas Heer <me@nheer.io>
2025-12-07 14:45:12 +0100

    perf(clojure): use doubles throughout to avoid long/double mixing
    3.6x speedup by avoiding clojure.lang.Numbers invocations.
    Fixes #104. Thanks @joinr for the optimization!

--- src/leibniz.clj
+++ src/leibniz.clj
@@ -3,14 +3,15 @@
 (set! *unchecked-math* :warn-on-boxed)
 
 (defn calc-pi-leibniz
-  "Translation of Java solution to Clojure"
+  "Translation of Java solution to Clojure - uses doubles throughout to avoid long/double mixing"
+  ^double
   [^long rounds]
-  (let [end (+ 2 rounds)]
-    (loop [i 2 x 1.0 pi 1.0]
-      (if (= i end)
-        (* 4 pi)
+  (let [end (double (+ 2 rounds))]
+    (loop [i 2.0 x 1.0 pi 1.0]
+      (if (== i end)
+        (* 4.0 pi)
         (let [x (- x)]
-          (recur (inc i) x (+ pi (/ x (dec (* 2 i))))))))))
+          (recur (inc i) x (+ pi (/ x (dec (* 2.0 i))))))))))
 
 (defn parse-int [s]
   (Integer/parseInt (re-find #"\A-?\d+" s)))
