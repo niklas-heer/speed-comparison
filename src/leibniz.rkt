cee1d5f35ba90119105d230c1aa892094bd57e4d
ShalokShalom <shalokshalom@protonmail.ch>
2025-12-08 08:29:32 +0100

    Optimize pi calculation using unsafe operations
    Refactor Leibniz formula implementation to use unsafe operations and improve performance.
    
    I asked someone from the Racket community to improve the existing code, as it seemed to be very out of fashion fro Racket to be so slow before.
    
    They meant, this here is more idiomatic.

--- src/leibniz.rkt
+++ src/leibniz.rkt
@@ -1,14 +1,16 @@
-#lang racket/base
-(require racket/string racket/file)
-
-(define rounds (string->number (string-trim (file->string "rounds.txt"))))
-
-(define pi 1.0)
-(define x 1.0)
-
-(for ([i (in-range 2 (+ rounds 2))])
-  (set! x (- x))
-  (set! pi (+ pi (/ x (- (* 2 i) 1)))))
-
-(set! pi (* pi 4.0))
-(displayln (real->decimal-string pi 16))
+(module* three #f
+  (#%declare #:unsafe)
+  (require (rename-in racket/unsafe/ops
+                      [unsafe-fl- fl-]
+                      [unsafe-fl+ fl+]
+                      [unsafe-fl/ fl/]
+                      [unsafe-fl* fl*])
+           #;racket/flonum)
+  (time
+   (let ()
+     (define pi
+       (for/fold ([pi 1.0] [x 1.0] #:result (* pi 4.0))
+                 ([i (in-range 2.0 (+ rounds 2.0))])
+         (let ([x (fl- x)])
+           (values (fl+ pi (fl/ x (fl- (fl* 2.0 i) 1.0))) x))))
+     (displayln (list 'three (real->decimal-string pi 16))))))
