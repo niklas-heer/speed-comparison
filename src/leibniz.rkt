e2691d156120a7f332862fc0d4b41e3c171525b3
ShalokShalom <shalokshalom@protonmail.ch>
2025-12-11 23:44:42 +0100

    Refactor Leibniz calculation to use 'pie' variable

--- src/leibniz.rkt
+++ src/leibniz.rkt
@@ -1,16 +1,15 @@
-(module* three #f
-  (#%declare #:unsafe)
-  (require (rename-in racket/unsafe/ops
-                      [unsafe-fl- fl-]
-                      [unsafe-fl+ fl+]
-                      [unsafe-fl/ fl/]
-                      [unsafe-fl* fl*])
-           #;racket/flonum)
-  (time
-   (let ()
-     (define pi
-       (for/fold ([pi 1.0] [x 1.0] #:result (* pi 4.0))
-                 ([i (in-range 2.0 (+ rounds 2.0))])
-         (let ([x (fl- x)])
-           (values (fl+ pi (fl/ x (fl- (fl* 2.0 i) 1.0))) x))))
-     (displayln (list 'three (real->decimal-string pi 16))))))
+#lang racket/base
+
+(require racket/file
+         racket/flonum)
+
+(define rounds (file->value "rounds.txt"))
+
+; use "pie" instead of "pi" to avoid confusion with racket's included "pi"
+(let ([stop (fl+ (exact->inexact rounds) 2.0)])
+  (let loop ([x (fl+ -1.0)]
+             [pie (fl+ 1.0)]
+             [i 2.0])
+    (if (fl>= i stop)
+        (displayln (real->decimal-string (* pie 4.0) 16))
+        (loop (fl- x) (fl+ pie (fl/ x (fl- (fl* 2.0 i) 1.0))) (fl+ i 1.0)))))
