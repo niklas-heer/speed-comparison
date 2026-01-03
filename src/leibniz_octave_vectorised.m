37446f454d13325c085a0afe3f2f5181594549d9
Fred Sonnenwald <142429179+fredsonnenwald@users.noreply.github.com>
2026-01-03 13:04:48 +0000

    fix(octave): work for even or odd numbers of rounds

--- src/leibniz_octave_vectorised.m
+++ src/leibniz_octave_vectorised.m
@@ -2,7 +2,7 @@
 
 rounds = csvread('rounds.txt');
 
-pi = 4 * sum(1 ./ (1 - 2 * rounds : 4 : 2 * rounds));
+pi = 4 * sum(1 ./ ((1 + mod(rounds,2) * 2 - 2 * rounds) : 4 : 2 * rounds));
 
 fprintf('%0.16f\n', pi)
 
